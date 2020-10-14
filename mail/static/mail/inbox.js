ee1834cbcb37799808803d098ee912254e2464bf
Jakub Serwatka <kubaserw@gmail.com>
2020-10-14 10:43:37 +0200

    email sending handling (sendEmailHandling()) added

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -8,7 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+  // Add send/compose handling
+  sendEmailHandling();
 });
+
+
+
+
+
+
 
 function compose_email() {
 
@@ -30,4 +39,42 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  // Get emails
+}
+
+// Send email function
+function sendEmailHandling() {
+  document.querySelector("#compose-form").addEventListener("submit", (event) => {
+    // Force not to reload
+    event.preventDefault();
+
+    // Post an email
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+      })
+    })
+    // Convert response to json
+    .then(response => response.json())
+    // Show response or throw error
+    .then(result => {
+      //Throw error if there is one
+      if (Object.keys(result)[0] === "error") {
+        throw new Error(result.error);
+      }
+      // Go to inbox and show success message
+      else {
+        load_mailbox('inbox');
+        alert(result.message);
+      }       
+    })
+    // Handle error
+    .catch(error => {
+      alert(error);
+    })
+  });
 }
