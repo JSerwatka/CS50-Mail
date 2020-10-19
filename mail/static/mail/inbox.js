9c3a683b31bfc13159f4ce1a49c4035503ad879a
Jakub Serwatka <kubaserw@gmail.com>
2020-10-19 12:43:14 +0200

    comment in js file updated

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,6 +1,4 @@
-// TODO: Comments in js fix
 // TODO: Save history to use back/forward withou logging out
-// TODO: mark as unread option
 
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -16,25 +14,199 @@ document.addEventListener('DOMContentLoaded', function() {
   // Add send/compose handling
   sendEmailHandling();
 
-  //Backgroud change hangling
+  //Backgroud change handling
   backgroudControl();
 });
 
-// Creates new email block for mailbox view
+// ------ Shows mailbox ------ 
+function load_mailbox(mailbox) {
+  // Get main email container
+  let emailsView = document.querySelector("#emails-view");
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#details-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  emailsView.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  // Get emails
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      // For each mail - compose a div and inject it into emailsView
+      emails.forEach(email => {
+        newEmailBlock(email, emailsView, mailbox);
+      })
+
+      // Listen for archive-icon click -> archive an email v unarchive-icon -> unarchive
+      document.querySelectorAll(".archive-icon, .unarchive-icon").forEach(element => {
+        element.addEventListener("click", (event) => {
+          event.stopPropagation();
+          // Get email's id
+          let email = event.target.parentNode.parentNode;
+
+          // Run hide animation
+          email.style.animationPlayState = "running";
+
+          // After animation - archive an email
+          email.addEventListener("animationend", () => {
+            // Check if archive or unarchive
+            let toArchive = (event.target.parentNode.className === "archive-icon") ? true : false;
+            
+            // Archive/unarchive this email
+            archiveControl(email, event.target, toArchive);
+          });
+        })
+      });   
+  });
+}
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+
+// ------ Shows compose an email ------ 
+function compose_email(replyEmail=null) {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#details-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // If this is a reply - populate with data
+  if (replyEmail !== null) {
+    document.querySelector('#compose-recipients').value = replyEmail.sender;
+    // Body = sender's mail + timestamp
+    document.querySelector('#compose-body').value = `
+    " On ${replyEmail.timestamp} ${replyEmail.sender} wrote: 
+     ${replyEmail.body}"`;
+    // Add Re: only if it's a first reply
+    if (replyEmail.subject.slice(0,3) === "Re:") {
+      document.querySelector('#compose-subject').value = replyEmail.subject;
+    }
+    else {
+      document.querySelector('#compose-subject').value = "Re: " + replyEmail.subject;
+    }
+  }
+  // Else - clear out composition fields
+  else {
+    document.querySelector('#compose-recipients').value = '';
+    document.querySelector('#compose-subject').value = '';
+    document.querySelector('#compose-body').value = '';
+  }
+}
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+
+// ------ Shows email's details ------ 
+function openEmail(email) {
+ 
+  // Show the details-view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#details-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+
+  // Fill with email info
+  document.querySelector("#details-view").innerHTML = `
+    <div class="email-sender"><b>From:</b> ${email.sender}</div>
+    <div class="email-recipients"><b>To:</b> ${email.recipients.join("; ")}</div>
+    <div class="email-subject"><b>Subject:</b> ${email.subject}</div>
+    <div class="email-timestamp"><b>Timestamp:</b> ${email.timestamp}</div>
+    <button class="btn btn-sm btn-outline-primary" id="reply"><img src="${replyBlueIcon}">Reply</button>
+    <!--Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>-->
+    <hr>
+    <div class="email-body">${email.body}</div>`;
+
+  // Check email as read if not already
+  if (email.read === false){
+    fetch(`/emails/${email.id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        read: true
+      })
+    })
+    .then(response => {
+      if (response.status === 204) {
+        console.log(`email id:${email.id} is marked as read`)
+      } 
+      else {
+        throw new Error("Unknown error during email mark as read attampt")
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  let replyButton = document.querySelector("#reply");
+
+  // Change color of reply icon if it's hover state or not
+  replyButton.addEventListener("mouseover", () => {
+    replyButton.firstElementChild.src = `${replyWhiteIcon}`;
+  })
+
+  replyButton.addEventListener("mouseout", () => {
+    replyButton.firstElementChild.src = `${replyBlueIcon}`;
+  })
+
+  // Compose a reply template
+  replyButton.addEventListener("click", () => {
+    compose_email(email);
+  })
+}
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+
+// ------ Sends email ------ 
+function sendEmailHandling() {
+document.querySelector("#compose-form").addEventListener("submit", (event) => {
+  // Force not to reload
+  event.preventDefault();
+
+  // Post an email
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: document.querySelector('#compose-recipients').value,
+      subject: document.querySelector('#compose-subject').value,
+      body: document.querySelector('#compose-body').value
+    })
+  })
+  // Convert response to json
+  .then(response => response.json())
+  // Show response or throw error
+  .then(result => {
+    //Throw error if there is one
+    if (Object.keys(result)[0] === "error") {
+      throw new Error(result.error);
+    }
+    // Go to inbox and show success message
+    else {
+      load_mailbox('inbox');
+      alert(result.message);
+    }       
+  })
+  // Handle error
+  .catch(error => {
+    alert(error);
+  })
+});
+}
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+
+// ------ Creates new email block for mailbox view  ------ 
 function newEmailBlock(email, emailsView, mailbox) {
   // Create new container
   let emailBlock = document.createElement("div");
   emailBlock.className = "email-block";
   emailBlock.id = email.id;
   
-  //Check id read/unread and set proper class
+  // Check id read/unread and set proper class
   if (email.read) {
-    emailBlock.classList.add("read")
-    emailBlock.classList.remove("unread")
+    emailBlock.classList.add("read");
+    emailBlock.classList.remove("unread");
   } 
   else {
-    emailBlock.classList.add("unread")
-    emailBlock.classList.remove("read")
+    emailBlock.classList.add("unread");
+    emailBlock.classList.remove("read");
   }
 
   // Save recipiants/sender as a single variable
@@ -61,8 +233,9 @@ function newEmailBlock(email, emailsView, mailbox) {
   // Inject to main mailbox container
   emailsView.appendChild(emailBlock);
 }
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
 
-// Archive/Unarchive email
+// ------ Archives/Unarchives email ------ 
 function archiveControl(email, icon, toArchive) {
   // Update archive/unarchive status
   fetch(`/emails/${email.id}`, {
@@ -95,183 +268,9 @@ function archiveControl(email, icon, toArchive) {
     console.log(error)
   })
 }
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
 
-// Show compose email
-function compose_email(replyEmail=null) {
-
-  // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#details-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
-
-  // If this is a reply - populate with data
-  if (replyEmail !== null) {
-    document.querySelector('#compose-recipients').value = replyEmail.sender;
-    // Body = sender's mail + timestamp
-    document.querySelector('#compose-body').value = `
-    " On ${replyEmail.timestamp} ${replyEmail.sender} wrote: 
-     ${replyEmail.body}"`;
-    // Add Re: only if it's a first reply
-    if (replyEmail.subject.slice(0,3) === "Re:") {
-      document.querySelector('#compose-subject').value = replyEmail.subject;
-    }
-    else {
-      document.querySelector('#compose-subject').value = "Re: " + replyEmail.subject;
-    }
-  }
-  // Else - clear out composition fields
-  else {
-    document.querySelector('#compose-recipients').value = '';
-    document.querySelector('#compose-subject').value = '';
-    document.querySelector('#compose-body').value = '';
-  }
-
-
-}
-
-// Show mailbox
-function load_mailbox(mailbox) {
-  // Get main email container
-  let emailsView = document.querySelector("#emails-view");
-  
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#details-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'none';
-
-  // Show the mailbox name
-  emailsView.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-  
-  // Get emails
-  fetch(`/emails/${mailbox}`)
-  .then(response => response.json())
-  .then(emails => {
-      // For each mail - compose a div and inject it into emailsView
-      emails.forEach(email => {
-        newEmailBlock(email, emailsView, mailbox);
-      })
-
-      // Listen for archive-icon click -> archive an email v unarchive-icon -> unarchive
-      // TODO: trash logic inside newEmailBlock (than I can delete email id as block id)
-      document.querySelectorAll(".archive-icon, .unarchive-icon").forEach(element => {
-        element.addEventListener("click", (event) => {
-          event.stopPropagation();
-          // Get email's id
-          let email = event.target.parentNode.parentNode;
-
-          // Run hide animation
-          email.style.animationPlayState = "running";
-
-          // After animation - archive an email
-          email.addEventListener("animationend", () => {
-            // Check if archive or unarchive
-            let toArchive = (event.target.parentNode.className === "archive-icon") ? true : false;
-            
-            // Archive/unarchive this email
-            archiveControl(email, event.target, toArchive);
-          });
-        })
-      })
-         
-  });
-}
-
-// Show email
-function openEmail(email) {
- 
-    // Show the details-view and hide other views
-    document.querySelector('#emails-view').style.display = 'none';
-    document.querySelector('#details-view').style.display = 'block';
-    document.querySelector('#compose-view').style.display = 'none';
-
-
-    // Fill with email info
-    document.querySelector("#details-view").innerHTML = `
-      <div class="email-sender"><b>From:</b> ${email.sender}</div>
-      <div class="email-recipients"><b>To:</b> ${email.recipients.join("; ")}</div>
-      <div class="email-subject"><b>Subject:</b> ${email.subject}</div>
-      <div class="email-timestamp"><b>Timestamp:</b> ${email.timestamp}</div>
-      <button class="btn btn-sm btn-outline-primary" id="reply"><img src="${replyBlueIcon}">Reply</button>
-      <!--Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>-->
-      <hr>
-      <div class="email-body">${email.body}</div>`;
-
-    // Check email as read if not already
-    if (email.read === false){
-      fetch(`/emails/${email.id}`, {
-        method: "PUT",
-        body: JSON.stringify({
-          read: true
-        })
-      })
-      .then(response => {
-        if (response.status === 204) {
-          console.log(`email id:${email.id} is marked as read`)
-        } 
-        else {
-          throw new Error("Unknown error during email mark as read attampt")
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    }
-
-    let replyButton = document.querySelector("#reply");
-
-    // Change color of reply icon if it's hover state or not
-    replyButton.addEventListener("mouseover", () => {
-      replyButton.firstElementChild.src = `${replyWhiteIcon}`;
-    })
-
-    replyButton.addEventListener("mouseout", () => {
-      replyButton.firstElementChild.src = `${replyBlueIcon}`;
-    })
-
-    // Compose a reply template
-    replyButton.addEventListener("click", () => {
-      compose_email(email);
-    })
-
-}
-
-// Send email function
-function sendEmailHandling() {
-  document.querySelector("#compose-form").addEventListener("submit", (event) => {
-    // Force not to reload
-    event.preventDefault();
-
-    // Post an email
-    fetch('/emails', {
-      method: 'POST',
-      body: JSON.stringify({
-        recipients: document.querySelector('#compose-recipients').value,
-        subject: document.querySelector('#compose-subject').value,
-        body: document.querySelector('#compose-body').value
-      })
-    })
-    // Convert response to json
-    .then(response => response.json())
-    // Show response or throw error
-    .then(result => {
-      //Throw error if there is one
-      if (Object.keys(result)[0] === "error") {
-        throw new Error(result.error);
-      }
-      // Go to inbox and show success message
-      else {
-        load_mailbox('inbox');
-        alert(result.message);
-      }       
-    })
-    // Handle error
-    .catch(error => {
-      alert(error);
-    })
-  });
-}
-
-// Controls backgroud selection from dropdown menu
+// ------ Controls backgroud selection from dropdown menu ------ 
 function backgroudControl() {
   let bodyNode = document.querySelector("body");
   let checkbox = document.querySelector("#animation-checkbox");
@@ -308,12 +307,13 @@ function backgroudControl() {
           //
           break;      
         default:
-          console.log("invalid item")
+          console.log("invalid item");
           break;
       }
 
       bodyNode.className = selectedBg;
-      bodyNode.prepend(shoutout)
+      bodyNode.prepend(shoutout);
     });
   }); 
 }
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
